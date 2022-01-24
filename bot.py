d08ccb290b76ed69a1c0d2de86709ab4d97cab49
psxzz <m.martusevich@yandex.ru>
2022-01-24 22:32:03 +0300

    Fix: webhook

--- bot.py
+++ bot.py
@@ -6,9 +6,10 @@ from bot_init import dp, bot, API_TOKEN
 from handlers import messages, style_transfering
 
 
-WEBHOOK_URL = f"https://neural-st-bot.herokuapp.com/{API_TOKEN}"
-WEBAPP_HOST = 'localhost'
-WEBAPP_PORT = 3001
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.environ.get('PORT', 5000))
+WEBHOOK_PATH = F"/webhook/{API_TOKEN}"
+WEBHOOK_URL = f"https://neural-st-bot.herokuapp.com{WEBHOOK_PATH}"
 
 
 async def on_startup(dp):
@@ -24,7 +25,7 @@ async def on_shutdown(_):
 if __name__ == "__main__":
     executor.start_webhook(
         dispatcher=dp, 
-        webhook_path='', 
+        webhook_path=WEBHOOK_PATH, 
         on_startup=on_startup, 
         on_shutdown=on_shutdown,
         skip_updates=True,
